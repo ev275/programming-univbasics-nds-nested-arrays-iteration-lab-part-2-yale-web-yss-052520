a1d1f118b08c334e57d43631ddae8fe323907033
Enrique Valencia <enrique.valencia@yale.edu>
2020-05-21 03:03:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index] < lowest_number
-      src[row_index][element_index] = lowest_number
+      lowest_number = src[row_index][element_index]
     end
     element_index += 1
   end
